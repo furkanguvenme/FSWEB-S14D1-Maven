8edc7bf0e123a047c58854aac81854a06a0d2f15
furkanguvenme <furkanguven.me@gmail.com>
2024-07-29 15:37:21 +0300

    Tasks Complated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -43,5 +43,6 @@ public class Main {
 
         System.out.println("cuboid.volume= " + cuboid.getVolume());
 
+        System.out.println("---------------------------------------------------");
     }
 }
